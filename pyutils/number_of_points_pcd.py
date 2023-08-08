51cd9a5a26c0a2eee7465142a14a85b87a037825
sumanth nagulvancha <sumanthrao1997@gmail.com>
2023-08-08 14:28:50 +0200

    num_pts_pcd

--- pyutils/number_of_points_pcd.py
+++ pyutils/number_of_points_pcd.py
@@ -6,11 +6,14 @@ import sys
 def num_of_points(filename):
     pcd = o3d.io.read_point_cloud(filename)
     print(f"The lenght of pointcloud at {filename} is: ")
-    print('\033[1m' + str(np.shape(np.asarray(pcd.points))) + '\033[0m')
+    print("\033[1m" + str(np.shape(np.asarray(pcd.points))) + "\033[0m")
+
+
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        filename = sys.argv[1]
-        num_of_points(filename)
+    if len(sys.argv) > 1:
+        for filename in sys.argv[1:]:
+            num_of_points(filename)
         sys.exit()
-    else: print("more than one filename")
+    else:
+        print("input atleast one filename")
     sys.exit()
