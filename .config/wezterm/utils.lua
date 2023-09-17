12a25e90eff2c0f835bcb81d52044a2afcf4ca1d
sumanth nagulvancha <sumanthrao1997@gmail.com>
2023-09-17 17:52:54 +0200

    path to cwd if path has extension

--- .config/wezterm/utils.lua
+++ .config/wezterm/utils.lua
@@ -1,20 +1,34 @@
 local wezterm = require 'wezterm'
 local utils = {}
 
-function utils.get_font()
+-- Example usage:
+function utils.open_path_in_new_tab(file_path)
+  local function getFileExtension(path)
+    return path:match("^.+(%..+)$")
+  end
 
-end
+  local function getParentPathWithExtension(path)
+    if getFileExtension(path) ~= nil then
+      local lastSeparator = path:match("[/\\]([^/\\]+)$")
+      if lastSeparator then
+        return path:sub(1, -(#lastSeparator + 1))
+      else
+        return path
+      end
+    else
+      return path
+    end
+  end
 
-function utils.open_dir_in_new_tab(dir)
   local act = wezterm.action
   local action = act.SpawnCommandInNewTab {
-    cwd = dir,
+    cwd = getParentPathWithExtension(file_path),
     set_environment_variables = {
       TERM = 'screen-256color',
     },
     args = {
       'nvim',
-      dir,
+      file_path,
     },
   }
   return action
