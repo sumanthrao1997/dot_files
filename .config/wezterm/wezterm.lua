bf0b04339bda525e441b6d07363fe5f381468461
sumanth nagulvancha <sumanthrao1997@gmail.com>
2023-09-17 17:18:18 +0200

    chnaged gpu of wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -23,18 +23,23 @@ tab_bar.apply_to_config(config)
 --font size
 config.font_size = 16.0
 -- config.font_size = 22.0
+-- selecting descrete gpu for my situation.
+-- can grt the available gpus by using wezterm.gui.enumerate_gpus()
+for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
+  if gpu.backend == 'Vulkan' and gpu.device_type == 'DiscreteGpu' then
+    config.webgpu_preferred_adapter = gpu
+    config.front_end = 'WebGpu'
+    break
+  end
+end
 
 -- color scheme
--- local scheme = wezterm.get_builtin_color_schemes()['Abernathy']
--- scheme.background = 'black'
--- config.color_scheme = 'Abernathy'
--- wezterm.color.load_scheme("wezterm/theme.toml")
 config.color_scheme = "Abernathy_black"
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
-config.audible_bell = "Disabled"
--- config.font_size = 16.0
 config.tab_max_width = 30
 config.scrollback_lines = 10000
+
+config.audible_bell = "Disabled"
 --return
 return config
