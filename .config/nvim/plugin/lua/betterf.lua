de84760057bc5989f460f1bb8b1da82cb16959df
sumanthrao1997 <sumanthrao1997@gmail.com>
2024-09-03 10:37:40 +0200

    betterF plugin install

---
+++ .config/nvim/plugin/lua/betterf.lua
@@ -0,0 +1,12 @@
+require('betterf').setup({
+    -- all labels to use in order, the last character is used for accessing overflow indices
+    labels = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",";"},
+
+    -- the highlight color for the markers
+    color="#ff0000",
+
+    -- forward and backward search mappings
+    mappings={
+        "<leader>f", "<leader>F"
+    },
+})
